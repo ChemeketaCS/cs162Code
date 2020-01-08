233d46dd0720100fb48f9dcd436b7896300e595a
Andrew Scholer <ascholer@chemeketa.edu>
2020-01-08 08:47:54 -0800

    SimpleString - Improve documentation of initialization

--- Week01/SimpleString/main.cpp
+++ Week01/SimpleString/main.cpp
@@ -9,16 +9,18 @@ const int MAX_LENGTH = 256;
 //Simplistic represntation of a string using an array of chars
 class SimpleString {
 private:
-    char letters[MAX_LENGTH];   //storage for string
-    int length = 0;             //logical length of string
+    char letters[MAX_LENGTH] = {0};     //storage for string
+                                        // = {0} initializes with all null chars
+    int length = 0;                     //logical length of string
 
 public:
     SimpleString() {
         length = 0;
 
-        //Start with all null chars
-        for(int i = 0; i < MAX_LENGTH; i++)
-            letters[i] = 0;
+//        //If we did not initialize the array where it was declared
+//        // would need a loop to do so in constructor
+//        for(int i = 0; i < MAX_LENGTH; i++)
+//            letters[i] = 0;
     }
 
     void appendLetter(char c) {
