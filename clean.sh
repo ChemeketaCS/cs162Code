c0110833452505f046a1d8df8ca7ee9ccd2f2674
Andrew Scholer <ascholer@chemeketa.edu>
2026-01-07 13:25:26 -0800

    Remove old maintenance scripts

--- clean.sh
+++
@@ -1,5 +0,0 @@
-find -type d -name Debug -exec rm -rv {} +
-find -type d -name Release -exec rm -rv {} +
-find -type f -name *.pro.user -delete
-find -type f -name *.exe -delete
-find -type f -name *.o -delete
