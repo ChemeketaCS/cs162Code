1a70a606306bc87a5e4b6cd2671de51c6ab1b92e
Andrew Scholer <ascholer@chemeketa.edu>
2026-01-23 07:44:24 -0800

    Week04 Family tree updates

--- Week04/FamilyTree/main.cpp
+++ Week04/FamilyTree/main.cpp
@@ -6,31 +6,52 @@
 using namespace std;
 
 int main() {
-    Person p0("Anna");
-    Person p1("Bob");
-    Person p2("Carl");
-    Person p3("Diana");
-    Person p4("Erin");
-    Person p5("Fay");
-    Person p6("George");
-    Person p7("Henry");
+  Person p0("Anna");
+  Person p1("Bob");
+  Person p2("Carl");
+  Person p3("Diana");
+  Person p4("Erin");
+  Person p5("Fay");
+  Person p6("George");
+  Person p7("Henry");
 
-    // Set up family relationships
-    p1.setMother(&p0);
-    p2.setMother(&p0);
-    p3.setMother(&p0);
-    p4.setMother(&p3);
-    p5.setMother(&p4);
-    p6.setMother(&p5);
-    p7.setMother(&p5);
+  // Set up family relationships
+  p1.setMother(&p0);
+  p2.setMother(&p0);
+  p3.setMother(&p0);
+  p4.setMother(&p3);
+  p5.setMother(&p4);
+  p6.setMother(&p5);
+  p7.setMother(&p5);
 
-    // Print some Person objects
-    p0.print();
-    p5.print();
-    p7.print();
+  // Point to a person
+  Person* currentPerson = &p7;
 
-    p5.setName("Fiona");
-    cout << "After changing Fay's name to Fiona..." << endl;
-    p5.print();
-    p7.print();
+  Person* mother = currentPerson->getMother();
+  cout << "Henry's mother: ";
+  mother->print();
+
+  Person* grandmother = mother->getMother();
+  cout << "Henry's grandmother: ";
+  grandmother->print();
+
+  Person* grandmothersChild = grandmother->getChild(0);
+  cout << "Henry's grandmother's first child: ";
+  grandmothersChild->print();
+
+  // start at Henry and walk up the family tree
+  currentPerson = &p7;
+  while (currentPerson != nullptr) {
+    // Print the current person's information
+    cout << "-------------------------" << endl;
+    cout << "Current Person address is " << currentPerson << endl;
+    cout << "They are: ";
+    currentPerson->print();
+    // Get the mother of the current person
+    Person* currentMother = currentPerson->getMother();
+    cout << "Their mother's address is " << currentMother << endl;
+    // Change the current person pointer to point to the mother
+    currentPerson = currentMother;
+  }
+  cout << "Reached the top of the family tree." << endl;
 }
