363e28d5a08a7de0b43848fc314af35dfc5d328f
Andrew Scholer <ascholer@chemeketa.edu>
2026-02-06 12:13:09 -0800

    w7 updates

--- Week07/Containers/PlayerList.h
+++ Week07/Containers/PlayerList.h
@@ -5,15 +5,15 @@
 
 class PlayerList {
 private:
-    std::string* m_players; // pointer to the array of player names
-    int m_size; // number of players in the list
+  std::string* m_players; // pointer to the array of player names
+  int m_size;             // number of players in the list
 public:
   PlayerList(int size);
 
   // Rule of Three:
-  ~PlayerList();  // Destructor
-  PlayerList(const PlayerList& other); // Copy constructor
-  PlayerList& operator=(const PlayerList& other);  // Assignment operator
+  ~PlayerList();                                  // Destructor
+  PlayerList(const PlayerList& other);            // Copy constructor
+  PlayerList& operator=(const PlayerList& other); // Assignment operator
 
   void setPlayerName(int index, const std::string& name);
 
