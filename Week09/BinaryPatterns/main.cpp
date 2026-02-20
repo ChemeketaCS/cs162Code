5e522c9dfc3cf65debbb86576cd952ab91185018
Andrew Scholer <ascholer@chemeketa.edu>
2026-02-20 11:47:47 -0800

    W09 binary updates

--- Week09/BinaryPatterns/main.cpp
+++ Week09/BinaryPatterns/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int binaryPatterns(int n) {
-    if (n == 0) {
-        return 1; // Base case: one pattern of length 0 (the empty pattern)
-    } else {
-        // Recursive case: for each pattern of length n-1, we can add '0' or '1'
-        return binaryPatterns(n - 1) + binaryPatterns(n - 1);
+void printBinaryStrings(const string& current, int remainingBits) {
+    if (remainingBits == 0) {
+        cout << current << endl;
+        return;
     }
+    printBinaryStrings(current + "0", remainingBits - 1);
+    printBinaryStrings(current + "1", remainingBits - 1);
+}
+
+// non-recursive wrapper
+void printBinaryStrings(int n) {
+    printBinaryStrings("", n);
 }
 
 int main() {
-    int count = binaryPatterns(3);
-    cout << "Number of binary patterns of length 3: " << count << endl;
+    printBinaryStrings(3);
 }
